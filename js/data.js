39e22f94b6f2016254153ffd9bce5dadbfe27eb8
phamttthao3004 <phamttthao3004@gmail.com>
2021-10-02 21:59:42 +0700

    Add files via upload
    home

---
+++ js/data.js
@@ -0,0 +1,112 @@
+var organisation = [
+    {
+        "id": 0,
+        "photo": "./images/logos/tesla-logo-png-2251.png",
+        "name": "Tesla Company",
+        "area": "California",
+        "url": ""
+    },
+    {
+        "id": 1,
+        "photo": "./images/logos/6603780637514882-Deloitte.png",
+        "name": "Deloitte",
+        "area": "London",
+        "url": ""
+    },
+    {
+        "id": 2,
+        "photo": "./images/logos/907559037421612-Microsoft.png",
+        "name": "Microsoft",
+        "area": "Washington",
+        "url": ""
+    },
+    {
+        "id": 3,
+        "photo": "./images/logos/22574426504815803-Salesforce.jpg",
+        "name": "Salesforce",
+        "area": "California",
+        "url": ""
+    },
+    {
+        "id": 4,
+        "photo": "./images/logos/5993846155016747-Amazon.png",
+        "name": "Amazone",
+        "area": "California",
+        "url": ""
+    }
+];
+var experts = [
+    {
+        "id": 0,
+        "photo": "./images/avatar/9226014915803886-Female_18.jpg",
+        "name": "Fabio Ramos",
+        "area": "University of Sydney",
+        "url": ""
+    },
+    {
+        "id": 1,
+        "photo": "./images/avatar/012235256338216649-Female_19.jpg",
+        "name": "Thomas G. Dietterich",
+        "area": "Amazon Web Services",
+        "url": ""
+    },
+    {
+        "id": 2,
+        "photo": "./images/avatar/40013598437633613-Male_18.jpg",
+        "name": "Stephan Chalup",
+        "area": "University of Newcastle",
+        "url": ""
+    },
+    {
+        "id": 3,
+        "photo": "./images/avatar/6337420440278503-Female_25.jpg",
+        "name": "Peter Corke",
+        "area": "Queensland University of Tech...",
+        "url": ""
+    },
+    {
+        "id": 4,
+        "photo": "./images/avatar/5682418048070823-Female_22.jpg",
+        "name": "Adam Le",
+        "area": "Melbourne University",
+        "url": ""
+    }
+];
+var technologies = [
+    {
+        id: 0,
+        name: "Computer Vision",
+        link: ""
+    },
+    {
+        id: 1,
+        name: "Data Mining",
+        link: ""
+    },{
+        id: 2,
+        name: "Database",
+        link: ""
+    },{
+        id: 3,
+        name: "Data Warehouse",
+        link: ""
+    },{
+        id: 4,
+        name: "Machine Learning",
+        link: ""
+    },{
+        id: 5,
+        name: "Artificial Intelligence",
+        link: ""
+    },
+    {
+        id: 6,
+        name: "Neural Network",
+        link: ""
+    },
+    {
+        id: 7,
+        name: "Natural Language Processing",
+        link: ""
+    }
+]
